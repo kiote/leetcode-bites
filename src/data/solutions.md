2b0bda98e125763c1d9ac928fcda6f070b5a4214
Ekaterina Krivich <krivich@tlu.ee>
2025-03-23 20:41:40 +0200

    refactor: update test cases for unique substring function to use JSON.stringify for comparison; add solutions documentation

---
+++ src/data/solutions.md
@@ -0,0 +1,54 @@
+## For debugging
+
+### Problem 1
+
+```
+def has_all_unique_chars(s):
+	uniq_map = {}
+	for c in s:
+		if c in uniq_map:
+			print(c)
+			return False
+		else:
+			uniq_map[c] = 1
+	return True
+```
+
+### Problem 2
+
+```
+def has_all_unique_chars(s):
+	uniq_map = {}
+	for c in s:
+		if c in uniq_map:
+			print(c)
+			return False
+		else:
+			uniq_map[c] = 1
+	return True
+	
+def is_substring_unique(s, start, end):
+    return has_all_unique_chars(s[start:end+1])
+``` 	
+
+### Problem 3
+
+```
+def all_uniq(s):
+	uniq_map = {}
+	for c in s:
+		if c in uniq_map:
+			return False
+		else:
+			uniq_map[c] = 1
+	return True
+
+def find_all_unique_substrings(s):
+	all_subs = set([])
+	for i in range(0, len(s)+1):
+		for j in range(i+1, len(s)+1):
+			if all_uniq(s[i:j]):
+				all_subs.add(s[i:j])
+			
+	return sorted(list(all_subs))
+```
--- src/data/testCases.js
+++ src/data/testCases.js
@@ -55,7 +55,7 @@ export const allTests = {
     {
       id: 1,
       name: 'Find all unique substrings in short string',
-      testCode: 'sorted(find_all_unique_substrings("abc")) == sorted(["a", "ab", "abc", "b", "bc", "c"])',
+      testCode: 'JSON.stringify(sorted(find_all_unique_substrings("abc"))) === JSON.stringify(sorted(["a", "ab", "abc", "b", "bc", "c"]))',
       functionName: 'find_all_unique_substrings',
       inputs: ['abc'],
       expectedOutput: ["a", "ab", "abc", "b", "bc", "c"]
@@ -63,7 +63,7 @@ export const allTests = {
     {
       id: 2,
       name: 'Find all unique substrings with duplicates',
-      testCode: 'sorted(find_all_unique_substrings("abb")) == sorted(["a", "ab", "b"])',
+      testCode: 'JSON.stringify(sorted(find_all_unique_substrings("abb"))) === JSON.stringify(sorted(["a", "ab", "b"]))',
       functionName: 'find_all_unique_substrings',
       inputs: ['abb'],
       expectedOutput: ["a", "ab", "b"]
@@ -71,7 +71,7 @@ export const allTests = {
     {
       id: 3,
       name: 'Test with empty string',
-      testCode: 'len(find_all_unique_substrings("")) == 0',
+      testCode: 'find_all_unique_substrings("").length === 0',
       functionName: 'find_all_unique_substrings',
       inputs: [''],
       expectedOutput: []
